dfde67b3588edbdf8b2cc7c400e66d79519f2596
Juan Rangel Juarez <jrj_juan@hotmail.com>
2013-06-03 09:09:39 -0500

    controladorfixture

--- application/controllers/fixture.php
+++ application/controllers/fixture.php
@@ -1,50 +1,34 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class fixture extends CI_Controller {
-		public function usuarios() {
+		public function anotador() {
 				$this->load->database();
-				$this->load->library('table');
-				$usuarios=$this->db->get('usuarios');
 				$usuarios = array(
 					array(
-					'id' => 17,
-					'nombre' => 'juan',
-					'email' => 'juan@juan.com',
+					'idannotador' => 17,
+					'idevent' => '235',
+					'idparticipant' => '56',
+					'annotations' => '2',
+					'minute' => '45',
 					),
 					array(
-					'id' => 18,
-					'nombre' => 'juan2',
-					'email' => 'juan2@juan.com',
+					'idannotador' => 18,
+					'idevent' => '236',
+					'idparticipant' => '57',
+					'annotations' => '3',
+					'minute' => '46',					
 					),
 					array(
-					'id' => 19,
-					'nombre' => 'juan3',
-					'email' => 'juan3@roberto.com',
+					'idannotador' => 19,
+					'idevent' => '237',
+					'idparticipant' => '58',
+					'annotations' => '4',
+					'minute' => '47',
 					),			
 				);
-				$this->db->insert_batch('usuarios', $usuarios);
+				$this->db->insert_batch('annotador', $usuarios);
 		}
-		public function publicaciones(){
-				$this->load->database();
-				$publicaciones=$this->db->get('publicaciones');
-				$publicaciones=array(
-					array(
-					'id' => 8,
-					'nombre' => 'juan',
-					'contenido' => 'Este es un archivo de texto',
-					),
-					array(
-					'id' => 9,
-					'nombre' => 'juan2',
-					'contenido' => 'Este es un archivo de texto2',					),
-					array(
-					'id' => 10,
-					'nombre' => 'juan3',
-					'contenido' => 'Este es un archivo de texto3',
-					),
-		    );
-			$this->db->insert_batch('publicaciones', $publicaciones);			
-		}
+		
 		public function index() {
 			if(ENVIRONMENT =='development') {
 				$this->usuarios();
